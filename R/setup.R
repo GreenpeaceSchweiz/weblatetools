13e797b8950a8b54c9c5af0917299735d255f546
Simon <simon@emperor.ch>
2022-09-15 10:25:33 +0200

    add httr:verbose option

--- R/setup.R
+++ R/setup.R
@@ -6,13 +6,17 @@ wenv <- new.env(parent = emptyenv())
 #' @param api.url The URL of your API entry point. Usually ""http://example.com/api/".
 #' @param token Your authorization token. See https://docs.weblate.org/en/latest/api.html#authentication-and-generic-parameters
 #' @param to.project The project you want to upload the translation files to. CAREFUL: make sure to set this to the appropriate project. It is set here instead of in copyTranslations() or postFile() to avoid mistakenly writing to the wrong project.
+#' @param debug Controls httr verbose logging (httr::verbose()). Defaults to FALSE
 #'
 #' @return
 #' @export
-setup <- function(api.url, token, to.project){
+setup <- function(api.url, token, to.project, debug = FALSE){
   wenv$BASE_URL <- api.url
   wenv$TOKEN <- paste("Token", token, sep = " ")
   wenv$TO_PROJECT <- to.project
+  if(debug){
+    httr::set_config(httr::verbose())
+  }
 }
 
 logger <- function(verbose, message) {
